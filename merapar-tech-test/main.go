6a43a7696629f8d709ee0a3dedde92ce37978092
Gareth Wood <gareth.wuud@icloud.com>
2023-11-23 16:06:12 +0000

    Cleanup

--- merapar-tech-test/main.go
+++ merapar-tech-test/main.go
@@ -7,30 +7,31 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/xyproto/randomstring"
+
+	random "github.com/xyproto/randomstring"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	t, err := template.New("index").Parse(`<h1>The Saved String Is {{ . }}!</h1>`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var rendered strings.Builder
-	err = t.Execute(&rendered, randomstring.HumanFriendlyString(10))
+	var b strings.Builder
+	err = t.Execute(&b, random.HumanFriendlyString(10))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	return events.APIGatewayProxyResponse{
+		Body:       b.String(),
+		StatusCode: 200,
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
-		Body:       rendered.String(),
-		StatusCode: 200,
 	}, nil
-}
-
-func main() {
-	lambda.Start(handler)
 }
